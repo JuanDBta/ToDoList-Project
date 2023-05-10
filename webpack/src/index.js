22b62f8d0392447fe3a00fb4c817a935243ee40c
JuanDBta <juandavid_diaz@hotmail.com>
2023-05-10 16:26:59 -0500

    Linters installed successfully

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,13 +3,12 @@ import './style.css';
 import printMe from './print.js';
 
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['To Do', 'List'], ' ');
-    element.classList.add('main_title');
-    
-    
-    element.appendChild('list_container');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['To Do', 'List'], ' ');
+  element.classList.add('main_title');
+
+  element.appendChild('list_container');
+  return element;
+}
+
+document.body.appendChild(component());
