42773ea98f70c62cd11cf93c07f6ec4b65233f9a
JuanDBta <juandavid_diaz@hotmail.com>
2023-05-10 22:41:50 -0500

    Create task object, array and function in index.js

--- webpack/dist/print.bundle.js
+++ webpack/dist/print.bundle.js
@@ -12,7 +12,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (/* binding */ printMe)
 /* harmony export */ });
 function printMe() {
-  console.log('I get called from print.js!');
 }
 
 /***/ })
@@ -23,4 +22,4 @@ function printMe() {
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZTtBQUNmO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLy4vc3JjL3ByaW50LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIHByaW50TWUoKSB7XG4gIGNvbnNvbGUubG9nKCdJIGdldCBjYWxsZWQgZnJvbSBwcmludC5qcyEnKTtcbn0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZTtBQUNmIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VicGFjay8uL3NyYy9wcmludC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBwcmludE1lKCkge1xufSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,13 +1,38 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['To Do', 'List'], ' ');
-  element.classList.add('main_title');
+const tasks = [];
 
-  element.appendChild('list_container');
-  return element;
-}
+const newTask1 = {
+  description: 'New task 1',
+  completed: false,
+  index: 0,
+};
 
-document.body.appendChild(component());
+const newTask2 = {
+  description: 'New task 2',
+  completed: false,
+  index: 1,
+};
+
+const newTask3 = {
+  description: 'New task 3',
+  completed: false,
+  index: 2,
+};
+
+tasks.push(newTask1);
+tasks.push(newTask2);
+tasks.push(newTask3);
+
+const addNewTask = () => {
+  const tasksList = document.getElementById('tasks');
+  tasks.forEach((task) => {
+    const element = document.createElement('li');
+    element.innerHTML = task.description;
+    tasksList.appendChild(element);
+  });
+};
+
+addNewTask();
